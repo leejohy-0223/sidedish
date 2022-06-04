4542ca1a939b2aa2e90a9434f9e15215f09f72c2
leejohy-0223 <leejohy@naver.com>
2022-06-04 11:19:44 +0900

    feat: add healthChecker

---
+++ backend/src/main/java/com/codesquad/sidedish/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.codesquad.sidedish;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/api/check")
+    public String healthChecker() {
+        return "ok";
+    }
+}
